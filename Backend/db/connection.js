4616abb9fbe1bc31af98fc78d32afac2a22fe5da
Mayur Suthar <128043340+mayursuthar02@users.noreply.github.com>
2024-04-30 11:28:48 +0530

    Update connection.js

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config(); // Load environment variables from .env file
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URL);
 .then(() => {
     console.log('Connected to MongoDB');
 })
